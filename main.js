c63bb2b5abda4b4881032e87185caeb410990d3e
ari <ariel@shuttle99.com>
2024-05-18 16:40:47 -0300

    first attempt to animate

--- main.js
+++ main.js
@@ -9,32 +9,21 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.setZ(30);
+
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.setZ(30);
+// this lets me control the scene view with the mouse
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const cubeSize = 4;
-const squaresOffset = 1.025;
+const squaresOffset = 1.05;
 const faceSize = cubeSize * 0.999; // shrinking it a little bit so that the edges are visible
-
-function createCube(x = 0, y = 0, z = 0) {
-  const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-  const material = new THREE.MeshBasicMaterial(0xffffff);
-  const cube = new THREE.Mesh(geometry, material);
-  // cube.rotateX(0.5)
-  // cube.rotateY(0.5)
-  cube.position.set(x, y, z);
-  const edgesGeometry = new THREE.EdgesGeometry(geometry);
-  const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000 }); // Black color for edges
-  const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial);
-  cube.add(edges); // Add edges to the cube
-  return cube;
-}
+let rotate = false;
 
 const colors = {
   white: 0xffffff,
@@ -44,6 +33,18 @@ const colors = {
   red: 0xff3333,
   orange: 0xffa500,
 };
+
+function createCube() {
+  const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+  const material = new THREE.MeshBasicMaterial(0xffffff);
+  const cube = new THREE.Mesh(geometry, material);
+  // might add edges later
+  const edgesGeometry = new THREE.EdgesGeometry(geometry);
+  const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000 }); // Black color for edges
+  const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial);
+  cube.add(edges); // Add edges to the cube
+  return cube;
+}
 
 function createFace(color) {
   const geometry = new THREE.BoxGeometry(faceSize, faceSize, 0.1);
@@ -107,7 +108,8 @@ function setFace(cube, color, facePosition) {
 }
 
 function createSquare(x, y, z) {
-  const cube = createCube(x, y, z, colors[0]);
+  // returns a group object with the cube and (for now) 6 color faces
+  const cube = createCube();
   // to improve:
   // I don't need to paint all faces of all squares.
   // to solve this I would need a face adjacency map (which I'll probably need in the future anyways)
@@ -127,40 +129,42 @@ function createSquare(x, y, z) {
   group.add(bottomFace);
   scene.add(group);
   // const box = new THREE.BoxHelper(group, 0xffff00);
-  // scene.add(box);  
-
+  // scene.add(box);
+  group.position.set(x, y, z);
   return group;
 }
 
-const controls = new OrbitControls(camera, renderer.domElement);
-const cubes = [];
-
-const cubeCoordinates = [];
-for (let x = -1; x < 2; x++) {
-  for (let y = -1; y < 2; y++) {
-    for (let z = -1; z < 2; z++) {
-      if (x === 0 && y === 0 && z === 0) {
-        continue;
+function createCoordinates() {
+  const cubeCoordinates = [];
+  for (let x = -1; x < 2; x++) {
+    for (let y = -1; y < 2; y++) {
+      for (let z = -1; z < 2; z++) {
+        if (x === 0 && y === 0 && z === 0) {
+          continue;
+        }
+        cubeCoordinates.push([x, y, z]);
       }
-      cubeCoordinates.push([x, y, z]);
     }
   }
+  return cubeCoordinates;
 }
 
-cubeCoordinates.forEach((coordinates) => {
-  cubes.push(
-    createSquare(
-      cubeSize + cubeSize * squaresOffset * coordinates[0],
-      cubeSize + cubeSize * squaresOffset * coordinates[1],
-      cubeSize + cubeSize * squaresOffset * coordinates[2]
-    )
-  );
-});
+function createRubiksCube() {
+  const cubes = [];
+  const cubeCoordinates = createCoordinates();
+  cubeCoordinates.forEach((coordinates) => {
+    cubes.push(
+      createSquare(
+        cubeSize * squaresOffset * coordinates[0],
+        cubeSize * squaresOffset * coordinates[1],
+        cubeSize * squaresOffset * coordinates[2]
+      )
+    );
+  });
+  return cubes;
+}
 
-cubes.forEach((cube) => {
-  scene.add(cube);
-});
-
+// will probably discard this function
 function splitFaces(cubes) {
   const rightFace = [];
   const leftFace = [];
@@ -170,12 +174,12 @@ function splitFaces(cubes) {
   const backFace = [];
 
   cubes.forEach((cube) => {
-    if (cube.position.x > 0){
+    if (cube.position.x > 0) {
       rightFace.push(cube);
     }
     if (cube.position.x < 0) {
       leftFace.push(cube);
-      }
+    }
     if (cube.position.y > 0) {
       topFace.push(cube);
     }
@@ -189,15 +193,104 @@ function splitFaces(cubes) {
       backFace.push(cube);
     }
   });
-
   return [rightFace, leftFace, topFace, bottomFace, frontFace, backFace];
 }
 
-console.log(splitFaces(cubes));
+const cubes = createRubiksCube();
+const group = new THREE.Group();
+cubes.forEach((cube) => {
+  group.add(cube);
+});
+scene.add(group);
+
+let faceGroup;
+let orientation = 1;
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
+  if (rotate) {
+    faceGroup.rotation.x += (Math.PI / 128) * orientation;
+    if (
+      Math.round(faceGroup.rotation.x * 100) %
+        Math.round((Math.PI / 2) * 100) ===
+      0
+    ) {
+      rotate = false;
+      console.log("stop");
+    }
+  }
 }
+
+// generic rotation toggle. Will probably drop this after implementing face-specific rotation
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    if (rotate) {
+      rotate = false;
+    } else {
+      rotate = true;
+    }
+  }
+});
+
+// rotate right face counter clockwise
+document.addEventListener("keydown", (event) => {
+  if (event.code === "KeyR") {
+    orientation = -1;
+    let faces = splitFaces(cubes);
+    console.log(faces[0]);
+    faceGroup = new THREE.Group();
+    faces[0].forEach((cube) => {
+      faceGroup.add(cube);
+    });
+    scene.add(faceGroup);
+    rotate = true;
+  }
+});
+
+// rotate right face clockwise
+document.addEventListener("keydown", (event) => {
+  if (event.shiftKey && event.code === "KeyR") {
+    console.log("R'");
+    orientation = 1;
+    let faces = splitFaces(cubes);
+    faceGroup = new THREE.Group();
+    faces[0].forEach((cube) => {
+      faceGroup.add(cube);
+    });
+    scene.add(faceGroup);
+    rotate = true;
+  }
+});
+
+// rotate left face clockwise
+document.addEventListener("keydown", (event) => {
+  console.log(event.code);
+  if (event.code === "KeyL") {
+    orientation = 1;
+    let faces = splitFaces(cubes);
+    faceGroup = new THREE.Group();
+    faces[1].forEach((cube) => {
+      faceGroup.add(cube);
+    });
+    scene.add(faceGroup);
+    rotate = true;
+  }
+});
+
+// rotate left face counter-clockwise
+document.addEventListener("keydown", (event) => {
+  console.log(event.code);
+  if (event.shiftKey && event.code === "KeyL") {
+    orientation = -1;
+    let faces = splitFaces(cubes);
+    faceGroup = new THREE.Group();
+    faces[1].forEach((cube) => {
+      faceGroup.add(cube);
+    });
+    scene.add(faceGroup);
+    rotate = true;
+  }
+});
 
 animate();
